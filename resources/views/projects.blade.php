7b4c3742c071385502751013632b8107811240b2
DivaniNL <dylanvannierop@gmail.com>
2021-02-02 10:38:27 +0100

    pull

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -8,6 +8,15 @@
 	<meta name="description" 
 content="This is the personal Portfolio-website of Divani Development">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZWGMSG6K8G"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZWGMSG6K8G');
+</script>
 
 	<title>Some of my Work</title>
 </head>
