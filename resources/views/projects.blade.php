4f94bc7fca265ccc28e752973c191b5351e0e0f4
DivaniNL <dylanvannierop@gmail.com>
2021-05-17 13:27:59 +0200

    commit 2

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -129,7 +129,7 @@ content="This is the personal Portfolio-website of Divani Development">
 					<a href="//{{$werk->url}}">
 					<h2>{{$werk->title}}</h2>
         	    		<img class = "image_project" src="{{ asset('storage/images/'.$werk->file) }}" alt="Image"/>
-						<h2>{{$werk->title}}</h2>
+						<h2>{{$werk->description}}</h2>
 					</a> 
 					<a href="{{ route('werken.edit', $werk->id)}}" class="btn btn-lg btn-warning">Edit</a>
 					
